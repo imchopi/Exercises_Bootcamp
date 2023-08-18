49e80d2b24cfecddce7dec9ecdada01cf95151f9
imchopi <apchopito@gmail.com>
2023-08-18 17:33:36 +0200

    V1.0 RELEASE - "components moved in "components""

--- exercise1.1/src/index.js
+++ exercise1.1/src/index.js
@@ -1,30 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Header } from './components/Header'
+import { Content } from './components/Content'
+import { Total } from './components/Total'
+
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const Header = (props) => { return <h1>{props.course}</h1> }
-
-  const Content = () => {
-    return (
-      <div>
-        <p>{part1} {exercises1}</p>
-        <p>{part2} {exercises2}</p>
-        <p>{part3} {exercises3}</p>
-      </div>
-    )
-  }
-
-  const Total = () => {
-    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  }
 
   return (
     <div>
