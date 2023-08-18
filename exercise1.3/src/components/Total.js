8a4c049035cf4f23c5f07fef78354d54df79a61b
imchopi <apchopito@gmail.com>
2023-08-18 19:35:50 +0200

    RELEASE - "exercise1.3 done"

---
+++ exercise1.3/src/components/Total.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Total = ({ parts }) => {
+  const exercises1 = parts[0].exercises;
+  const exercises2 = parts[1].exercises;
+  const exercises3 = parts[2].exercises;
+
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+};
